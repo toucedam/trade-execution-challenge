ef179ab1830e66b455e4099e129dc9e2def7593d
toucedam <marina.touceda@liporide.com>
2021-11-14 17:04:11 +0100

    Redis queue mock and updated UTs

--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -1,11 +1,13 @@
 require "./src/clean_code/trade_execution_service.rb"
 require "./spec/mocks/http_request_service_mock.rb"
+require "./spec/mocks/redis_queue_service_mock.rb"
 
 RSpec.describe TradeExecutionService, "#routing" do
     context "Less than 10k USD" do
         it "routes to LIQUIDITY_PROVIDER_C" do
             httṕ_request_service = HttpRequestServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service)
+            queue_service = RedisQueueServiceMock.new
+            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service)
             execution_service.execute_order(
                 'buy', 9_999, 'USD', 'EUR',
                 '11/12/2018', '1.1345', 'X-A213FFL'
@@ -18,7 +20,8 @@ RSpec.describe TradeExecutionService, "#routing" do
     context "Equal or bigger than 10k USD but less than 100k USD" do
         it "routes to LIQUIDITY_PROVIDER_B if equal to 10k USD" do
             httṕ_request_service = HttpRequestServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service)
+            queue_service = RedisQueueServiceMock.new
+            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service)
             execution_service.execute_order(
                 'buy', 10_000, 'USD', 'EUR',
                 '11/12/2018', '1.1345', 'X-A213FFL'
@@ -29,7 +32,8 @@ RSpec.describe TradeExecutionService, "#routing" do
         
         it "routes to LIQUIDITY_PROVIDER_B if bigger than 10k USD but less than 100k USD" do
             httṕ_request_service = HttpRequestServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service)
+            queue_service = RedisQueueServiceMock.new
+            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service)
             execution_service.execute_order(
                 'buy', 99_999, 'USD', 'EUR',
                 '11/12/2018', '1.1345', 'X-A213FFL'
@@ -42,7 +46,8 @@ RSpec.describe TradeExecutionService, "#routing" do
     context "Bigger than 100k USD" do
         it "routes to LIQUIDITY_PROVIDER_A if equal to 100k USD" do
             httṕ_request_service = HttpRequestServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service)
+            queue_service = RedisQueueServiceMock.new
+            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service)
             execution_service.execute_order(
                 'buy', 10_0000, 'USD', 'EUR',
                 '11/12/2018', '1.1345', 'X-A213FFL'
@@ -53,7 +58,8 @@ RSpec.describe TradeExecutionService, "#routing" do
         
         it "routes to LIQUIDITY_PROVIDER_A if bigger than 100k USD" do
             httṕ_request_service = HttpRequestServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service)
+            queue_service = RedisQueueServiceMock.new
+            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service)
             execution_service.execute_order(
                 'buy', 5000_000, 'USD', 'EUR',
                 '11/12/2018', '1.1345', 'X-A213FFL'
