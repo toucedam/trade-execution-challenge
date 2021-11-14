6c4f64683ca2734fa26cdaf450dcec2d76462e23
toucedam <marina.touceda@liporide.com>
2021-11-14 18:56:13 +0100

    Use Currency Service in Liquidity Provider Factory

--- src/clean_code/trade_execution/liquidity_provider/liquidity_provider_factory.rb
+++ src/clean_code/trade_execution/liquidity_provider/liquidity_provider_factory.rb
@@ -1,35 +1,22 @@
-require "money"
-require "monetize"
-
-I18n.config.available_locales = :en
-Money.locale_backend = :i18n
-Money.rounding_mode = BigDecimal::ROUND_CEILING
-
 class LiquidityProviderFactory
-    USD = "USD"
-    
-    def initialize(httpRequestService, queueService, configService)
+    def initialize(httpRequestService, queueService, configService, currencyService)
         @httpRequestService = httpRequestService
         @queueService = queueService
         @configService = configService
+        @currencyService = currencyService
     end
 
     def get_liquidity_provider(size, currency)
-        amount = amount_in_usd(size, currency)
+        amount = @currencyService.amount_in_usd(size, currency)
 
-        if amount < 10_000.to_money(USD) 
+        if amount < @currencyService.amount_in_usd(10_000)
             lp = LiquidityProviderC.new(@httpRequestService, @configService)
-        elsif (amount >= 10_000.to_money(USD) && amount < 100_000.to_money(USD))
+        elsif (amount >= @currencyService.amount_in_usd(10_000) && amount < @currencyService.amount_in_usd(100_000))
             lp = LiquidityProviderB.new(@queueService)
         else
             lp = LiquidityProviderA.new(@queueService)
         end
 
         lp
-    end
-
-    def amount_in_usd(size, currency)
-      # it would return a Money object representing a USD amount
-      size.to_money(currency).to_money(USD)
     end
 end
