2cc19b28bda2c790a40a0c9d74502368aa73decf
toucedam <marina.touceda@liporide.com>
2021-11-14 18:00:24 +0100

    Update Routing UTs

--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -1,77 +1,65 @@
-require "./src/clean_code/trade_execution/trade_execution_service.rb"
+require "./src/clean_code/trade_execution/liquidity_provider/liquidity_provider_factory.rb"
+require "./src/clean_code/trade_execution/liquidity_provider/liquidity_provider_a.rb"
+require "./src/clean_code/trade_execution/liquidity_provider/liquidity_provider_b.rb"
+require "./src/clean_code/trade_execution/liquidity_provider/liquidity_provider_c.rb"
 require "./spec/mocks/http_request_service_mock.rb"
 require "./spec/mocks/redis_queue_service_mock.rb"
 require "./spec/mocks/config_service_mock.rb"
 
-RSpec.describe TradeExecutionService, "#routing" do
+RSpec.describe LiquidityProviderFactory, "#routing" do
     context "Less than 10k USD" do
         it "routes to LIQUIDITY_PROVIDER_C" do
-            httṕ_request_service = HttpRequestServiceMock.new
+            http_request_service = HttpRequestServiceMock.new
             queue_service = RedisQueueServiceMock.new
             config_service = ConfigServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service, config_service)
-            execution_service.execute_order(
-                'buy', 9_999, 'USD', 'EUR',
-                '11/12/2018', '1.1345', 'X-A213FFL'
-            );
+            liquidity_provider_factory = LiquidityProviderFactory.new(http_request_service, queue_service, config_service)
+            liquidity_provider = liquidity_provider_factory.get_liquidity_provider(9_999, 'USD');
 
-            expect(execution_service.lp).to eq execution_service.LIQUIDITY_PROVIDER_C
+            expect(liquidity_provider).to be_an_instance_of LiquidityProviderC
         end
     end
 
     context "Equal or bigger than 10k USD but less than 100k USD" do
-        it "routes to LIQUIDITY_PROVIDER_B if equal to 10k USD" do
-            httṕ_request_service = HttpRequestServiceMock.new
+        it "routes to LIQUIDITY_PROVIDER_B if equal to 10k USD" do        
+            http_request_service = HttpRequestServiceMock.new
             queue_service = RedisQueueServiceMock.new
             config_service = ConfigServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service, config_service)
-            execution_service.execute_order(
-                'buy', 10_000, 'USD', 'EUR',
-                '11/12/2018', '1.1345', 'X-A213FFL'
-            );
+            liquidity_provider_factory = LiquidityProviderFactory.new(http_request_service, queue_service, config_service)
+            liquidity_provider = liquidity_provider_factory.get_liquidity_provider(10_000, 'USD');
 
-            expect(execution_service.lp).to eq execution_service.LIQUIDITY_PROVIDER_B
+            expect(liquidity_provider).to be_an_instance_of LiquidityProviderB
         end        
         
-        it "routes to LIQUIDITY_PROVIDER_B if bigger than 10k USD but less than 100k USD" do
-            httṕ_request_service = HttpRequestServiceMock.new
+        it "routes to LIQUIDITY_PROVIDER_B if bigger than 10k USD but less than 100k USD" do      
+            http_request_service = HttpRequestServiceMock.new
             queue_service = RedisQueueServiceMock.new
             config_service = ConfigServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service, config_service)
-            execution_service.execute_order(
-                'buy', 99_999, 'USD', 'EUR',
-                '11/12/2018', '1.1345', 'X-A213FFL'
-            );
+            liquidity_provider_factory = LiquidityProviderFactory.new(http_request_service, queue_service, config_service)
+            liquidity_provider = liquidity_provider_factory.get_liquidity_provider(99_999, 'USD');
 
-            expect(execution_service.lp).to eq execution_service.LIQUIDITY_PROVIDER_B
+            expect(liquidity_provider).to be_an_instance_of LiquidityProviderB
         end
     end
 
     context "Bigger than 100k USD" do
         it "routes to LIQUIDITY_PROVIDER_A if equal to 100k USD" do
-            httṕ_request_service = HttpRequestServiceMock.new
+            http_request_service = HttpRequestServiceMock.new
             queue_service = RedisQueueServiceMock.new
             config_service = ConfigServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service, config_service)
-            execution_service.execute_order(
-                'buy', 10_0000, 'USD', 'EUR',
-                '11/12/2018', '1.1345', 'X-A213FFL'
-            );
+            liquidity_provider_factory = LiquidityProviderFactory.new(http_request_service, queue_service, config_service)
+            liquidity_provider = liquidity_provider_factory.get_liquidity_provider(10_0000, 'USD');
 
-            expect(execution_service.lp).to eq execution_service.LIQUIDITY_PROVIDER_A
+            expect(liquidity_provider).to be_an_instance_of LiquidityProviderA
         end        
         
         it "routes to LIQUIDITY_PROVIDER_A if bigger than 100k USD" do
-            httṕ_request_service = HttpRequestServiceMock.new
+            http_request_service = HttpRequestServiceMock.new
             queue_service = RedisQueueServiceMock.new
             config_service = ConfigServiceMock.new
-            execution_service = TradeExecutionService.new(httṕ_request_service, queue_service, config_service)
-            execution_service.execute_order(
-                'buy', 5000_000, 'USD', 'EUR',
-                '11/12/2018', '1.1345', 'X-A213FFL'
-            );
+            liquidity_provider_factory = LiquidityProviderFactory.new(http_request_service, queue_service, config_service)
+            liquidity_provider = liquidity_provider_factory.get_liquidity_provider(5000_000, 'USD');
 
-            expect(execution_service.lp).to eq execution_service.LIQUIDITY_PROVIDER_A
+            expect(liquidity_provider).to be_an_instance_of LiquidityProviderA
         end
     end
 end
