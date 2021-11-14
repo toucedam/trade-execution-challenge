7f88b5326eba25c6c0aa5eae47d9a7a11bf9d0be
toucedam <marina.touceda@liporide.com>
2021-11-14 17:05:28 +0100

    Simplify redis mock

--- spec/mocks/redis_queue_service_mock.rb
+++ spec/mocks/redis_queue_service_mock.rb
@@ -1,19 +1,4 @@
 class RedisQueueServiceMock
-    @queue
-    @redis_msg
-
-    def queue
-        @queue
-    end
-
-    def redis_msg
-        @redis_msg
-    end
-    
     def push_to_queue(queue, command, payload = nil)
-        redis_msg = payload == nil ? command : "#{command}::#{JSON.dump(payload)}" 
-        
-        @queue = queue
-        @redis_msg = redis_msg
     end
 end
