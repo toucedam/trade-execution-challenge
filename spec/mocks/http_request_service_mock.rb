62ca0930dac3cbb9c590653b0bed387250bf950e
toucedam <marina.touceda@liporide.com>
2021-11-14 18:32:20 +0100

    Set success on HttpRequestServiceMock

--- spec/mocks/http_request_service_mock.rb
+++ spec/mocks/http_request_service_mock.rb
@@ -1,11 +1,15 @@
 class HttpRequestServiceMock
     include HTTParty
   
+    def initialize(successful = true)
+        @successful = successful
+    end
+
     def post(url, payload)
         "response"
     end
 
     def is_successful_response(response)
-        true
+        @successful
     end
 end
