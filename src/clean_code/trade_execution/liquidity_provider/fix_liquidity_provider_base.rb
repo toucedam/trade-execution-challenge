891fe296ed2a1a0c01b47b4ee476872324fb9596
toucedam <marina.touceda@liporide.com>
2021-11-14 17:59:25 +0100

    Add Fix Liquidity Provider Base class

---
+++ src/clean_code/trade_execution/liquidity_provider/fix_liquidity_provider_base.rb
@@ -0,0 +1,13 @@
+class FixLiquidityProviderBase
+    def initialize(queueService)
+        @queueService = queueService
+    end
+
+    def wait_for_response(order_id)
+      # blocking read waiting for a redis key where trade confirmation is stored
+    end
+
+    def handle_trade_confirmation(rest_trade_confirmation)
+      # trade confirmation will be persisted in db
+    end
+end
